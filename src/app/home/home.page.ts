a1c3fd4ebb3638cb7961d10160516e203ef9e9d6
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-08-23 09:26:15 +0100

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  map?: L.Map
 
   constructor() {}
+
+  ionViewWillEnter(){
+    this.map = L.map('mapId').setView([41.4520951,2.2311724], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+    L.marker(
+      [41.4040129,2.1749179],
+      {icon: L.icon({iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png'})})
+      .addTo(this.map)
+    .bindPopup('A pretty place in barcelone')
+    .openPopup();
+
+    L.marker(
+      [41.4242926,2.1904551],
+      {icon: L.icon({iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png'})})
+      .addTo(this.map)
+    .bindPopup('A pretty place in barcelone')
+    .openPopup();
+
+  }
+
+  ngOnInit(): void {
+  }
 
 }
