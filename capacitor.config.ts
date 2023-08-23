fb7fe51e2a51f2fbad6f97ffe26738b70cad4c66
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-08-23 08:29:22 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tp-map-geo',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
